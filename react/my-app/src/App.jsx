2103f3009faeb9eef19d3d6b56d8fa9a00251f90
Kitsune Mao <animecreativityfandomuwu@GMAIL.COM>
2025-05-25 00:50:14 +0300

    Add files via upload

---
+++ react/my-app/src/App.jsx
@@ -0,0 +1,29 @@
+import "./assets/styles.css"
+
+export function App() {
+  return (
+    <header class="header">BMW Germany official store car garage 
+      <div>
+    <img src="\src\assets\img\icon9.png" alt="icon2" />
+    </div>
+    <img src="\src\assets\img\icon1.jpeg" alt="icon" />
+    <button class = "icon1">BMW M4</button>
+    <img src="\src\assets\img\ican2.jpeg" alt="icon" />
+    <button class = "icon1">BMW X6</button>
+    <img src="\src\assets\img\ican3.png" alt="icon" />
+    <button class = "icon1">BMW X5</button>
+    <img src="\src\assets\img\icon4.png" alt="icon" />
+    <button class = "icon1">BMW X4</button>
+    <img src="\src\assets\img\icon5.png" alt="icon" />
+    <button class = "icon1">BMW M3</button>
+    <img src="\src\assets\img\icon6.png" alt="icon" />
+    <button class = "icon1">BMW M3 2021</button>
+    <img src="\src\assets\img\icon7.jpeg" alt="icon" />
+    <button class = "icon1">BMW M3 E90</button>
+    <img src="\src\assets\img\icon8.png" alt="icon" />
+    <button class = "icon1">BMW M3 E46</button>
+    <div class="button1">
+    </div>
+    </header>
+    );
+}
